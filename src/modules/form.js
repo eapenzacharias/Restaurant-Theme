fa7587813f0f249bdaf45491232615cfe8d48da9
eapenzacharias <eapenzac@gmail.com>
2020-02-19 21:21:58 +0100

    researvation page added created form generator method

---
+++ src/modules/form.js
@@ -0,0 +1,30 @@
+function makeForm(formID, formAr) {
+  const form = document.createElement('form');
+  form.setAttribute('id', formID);
+  formAr.forEach(item => {
+    const group = document.createElement('div');
+    group.setAttribute('class', 'input-group mb-3');
+
+    const prependDiv = document.createElement('div');
+    prependDiv.setAttribute('class', 'input-group-prepend');
+
+    const span = document.createElement('span');
+    span.setAttribute('class', 'input-group-text');
+    span.setAttribute('id', `${item[1]}-input`);
+    span.innerHTML = item[0];
+    const textF = document.createElement('input');
+    textF.outerHTML = `<input type="${item[2]}" class="form-control" aria-describedby="${item[1]}-input">`;
+
+    prependDiv.appendChild(span);
+    group.appendChild(prependDiv);
+    group.appendChild(textF);
+    form.appendChild(group);
+  });
+  const btn = document.createElement('button');
+  btn.setAttribute('class', 'btn btn-outline-secondary');
+  btn.innerHTML = 'Submit';
+  form.appendChild(btn);
+  return form;
+}
+
+export default makeForm;
