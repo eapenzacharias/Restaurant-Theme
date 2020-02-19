d9b0aba180fc8d10cc92cbb5795a33a543fdcfc8
eapenzacharias <eapenzac@gmail.com>
2020-02-19 14:25:16 +0100

    json reader added to menu

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,8 +1,27 @@
 import setPage from "./setpage";
 
-function menu() {
+async function fetchData() {
+  const response = await fetch('./data/menu.json', { mode: 'cors' });
+  const data = await response.json();
+  return data;
+}
+
+function printMenu(type) {
+  const head = document.createElement('h2');
+  const icon = document.createElement('i');
+  icon.setAttribute('class', `${type.icon}`);
+  head.innerHTML = `${type.type}`;
+  head.appendChild(icon);
+  return head;
+}
+
+async function menu() {
   const container = setPage('menu-link');
-  container.innerHTML = 'Menu Page';
+  container.classList.add('menu');
+  let menu = await fetchData();
+  menu.data.forEach(element => {
+    container.appendChild(printMenu(element));
+  });
 }
 
 export default menu;
