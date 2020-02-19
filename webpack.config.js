01429c41f00035690e40a0aeb9ad8a41c0386b07
eapenzacharias <eapenzac@gmail.com>
2020-02-19 23:37:21 +0100

    lint error fix

--- webpack.config.js
+++ webpack.config.js
@@ -8,18 +8,18 @@ module.exports = {
   },
   module: {
     rules: [{
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-        ],
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader',
-        ],
-      },
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        'css-loader',
+      ],
+    },
+    {
+      test: /\.(png|svg|jpg|gif)$/,
+      use: [
+        'file-loader',
+      ],
+    },
     ],
   },
 };
