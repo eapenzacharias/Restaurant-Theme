1048f69192cd1cbd8c0b1d6cea5f2c7cb940a989
eapenzacharias <eapenzac@gmail.com>
2020-02-19 18:01:42 +0100

    page structure added to  reservation and contact

--- src/modules/contact.js
+++ src/modules/contact.js
@@ -2,7 +2,25 @@ import setPage from "./setpage";
 
 function contact() {
   const container = setPage('contact-link');
-  container.innerHTML = 'Contact Page';
+  container.setAttribute('class', 'container reserve');
+  const row = document.createElement('div');
+  row.setAttribute('class', 'row');
+
+  const colOne = document.createElement('div');
+  colOne.setAttribute('class', 'd-none d-lg-block d-xl-block col-lg-4 image');
+  colOne.style.backgroundImage = "url('./img/contact.jpg')";
+  const colTwo = document.createElement('div');
+  colTwo.setAttribute('class', 'col-12 col-lg-4 content');
+  const colThr = document.createElement('div');
+  colThr.setAttribute('class', 'col-12 col-lg-4 form');
+
+  colTwo.innerHTML = 'two';
+  colThr.innerHTML = 'Three'
+
+  row.appendChild(colOne);
+  row.appendChild(colTwo);
+  row.appendChild(colThr);
+  container.appendChild(row);
 }
 
 export default contact;
--- src/modules/reserve.js
+++ src/modules/reserve.js
@@ -2,7 +2,21 @@ import setPage from "./setpage";
 
 function reserve() {
   const container = setPage('reserve-link');
-  container.innerHTML = 'Reservation Page';
+  container.setAttribute('class', 'container reserve');
+  const row = document.createElement('div');
+  row.setAttribute('class', 'row');
+
+  const colOne = document.createElement('div');
+  colOne.setAttribute('class', 'd-none d-lg-block d-xl-block col-lg-7 image');
+  colOne.style.backgroundImage = "url('./img/tables.jpg')";
+  const colTwo = document.createElement('div');
+  colTwo.setAttribute('class', 'col-12 col-lg-5 form');
+
+  colTwo.innerHTML = 'two';
+
+  row.appendChild(colOne);
+  row.appendChild(colTwo);
+  container.appendChild(row);
 }
 
 export default reserve;
